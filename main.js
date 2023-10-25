891cf87768dccba7133c1b48d89dbd3ff0e0dad0
ruveydaakbolat <ruveydaakbolat3@gmail.com>
2023-10-25 21:55:54 +0300

    listing,search,auth

--- main.js
+++ main.js
@@ -1,12 +1,64 @@
+import { API } from "./scripts/api.js";
 import { getLocal } from "./scripts/helpers.js";
-import { renderUserInfo } from "./scripts/ui.js";
+import {
+  renderLoader,
+  renderUserInfo,
+  ele,
+  renderTimeline,
+  renderResults,
+} from "./scripts/ui.js";
+
+const user = getLocal("USER");
+
+const router = async () => {
+  const params = new URLSearchParams(location.search);
+  const page = params.get("page");
+  const query = params.get("q");
+
+  switch (page) {
+    // tweet detay
+    case "status":
+      break;
+    // arama sayfası
+    case "search":
+      renderLoader(ele.main);
+      const results = await API.fetchData(`/search.php?query=${query}`);
+      renderResults(results, ele.main);
+      break;
+    // kullanıcı detay sayfası
+    case "user":
+    //ana sayfayı ekrana bas
+    default:
+      renderLoader(ele.tweetsArea);
+
+      const data = await API.fetchData(
+        `/timeline.php?screenname=${user.profile}`
+      );
+
+      renderTimeline(data, ele.tweetsArea);
+      break;
+  }
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-  const user = getLocal("USER");
-  
-  if(user) {
+  if (user) {
     renderUserInfo(user);
   } else {
-    location = '/auth.html';
+    location = "/auth.html";
   }
+
+  router();
+});
+
+ele.logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("USER");
+  location = "/auth.html";
+});
+
+ele.form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const query = e.target[0].value;
+
+  location = `?page=search&q=${query}`;
 });
--- scripts/ui.js
+++ scripts/ui.js
@@ -2,11 +2,130 @@ export const ele = {
   user_name: document.getElementById("user-name"),
   user_tag: document.getElementById("user-tag"),
   pics: document.querySelectorAll("#profile-pic"),
+  tweetsArea: document.querySelector(".tweets-area"),
+  logoutBtn: document.querySelector("#logout-btn"),
+  form: document.querySelector("aside form"),
+  main: document.querySelector("main"),
 };
 
 export const renderUserInfo = (user) => {
-    ele.pics.forEach((img) => (img.src = user.avatar));
+  ele.pics.forEach((img) => (img.src = user.avatar));
 
-    ele.user_name.innerText = user.name;
-    ele.user_tag.innerText = '@' + user.profile;
+  ele.user_name.innerText = user.name;
+  ele.user_tag.innerText = "@" + user.profile;
+};
+
+export const renderLoader = (outlet) => {
+  outlet.innerHTML = `
+  <div class="d-flex justify-content-center my-5">
+    <div class="spinner-grow" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  `;
+};
+
+export const getMedia = (media) => {
+  if (media.photo) {
+    return `<img src="${media.photo[0].media_url_https}"/> `;
+  }
+
+  if (media.video) {
+    const mp4 = media.video[0].variants[0].url;
+    return `
+    <video controls>
+       <source src="${mp4}" /> 
+    </video>`;
+  }
+
+  return "";
+};
+
+export const renderTimeline = (data, outlet) => {
+  console.log(data);
+  outlet.innerHTML = data.timeline
+    .map(
+      (i) => `
+    <div class="tweet">
+          <img id="user-img" src="${data.user.avatar}" />
+          <div class="body">
+            <div class="user">
+              <div>
+                <b>${data.user.name}</b>
+                <p>${data.user.profile}</p>
+                <p>${moment(i.created_at).fromNow()}</p>
+              </div>
+              <i class="bi bi-three-dots"></i>
+            </div>
+            <div class="content">
+              <p>${i.text}</p>
+              ${getMedia(i.media)}
+            </div>
+            <div class="buttons">
+              <button>
+                <i class="bi bi-chat"></i>
+                <span>${i.replies}</span>
+              </button>
+              <button>
+                <i class="bi bi-recycle"></i>
+                <span>${i.retweets}</span>
+              </button>
+              <button>
+                <i class="bi bi-heart"></i>
+                <span>${i.favorites}</span>
+              </button>
+              <button>
+                <i class="bi bi-bookmark"></i>
+                <span>${i.bookmarks}</span>
+              </button>
+            </div>
+          </div>
+        </div>
+  `
+    )
+    .join("");
+};
+
+export const renderResults = (data, outlet) => {
+  outlet.innerHTML = data.timeline
+    .map(
+      (i) => `
+    <div class="tweet">
+      <img id="user-img" src="${i.user_info.avatar}" />
+      <div class="body">
+        <div class="user">
+          <div>
+            <b>${i.user_info.name}</b>
+            <p>@${i.user_info.screen_name}</p>
+            <p>${moment(i.created_at).fromNow()}</p>
+          </div>
+          <i class="bi bi-three-dots"></i>
+        </div>
+        <div class="content">
+          <p>${i.text}</p>
+          ${getMedia(i.media)}
+        </div>
+        <div class="buttons">
+          <button>
+            <i class="bi bi-chat"></i>
+            <span>${i.replies}</span>
+          </button>
+          <button>
+            <i class="bi bi-recycle"></i>
+            <span>${i.retweets}</span>
+          </button>
+          <button>
+            <i class="bi bi-heart"></i>
+            <span>${i.favorites}</span>
+          </button>
+          <button>
+            <i class="bi bi-bookmark"></i>
+            <span>${i.bookmarks}</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  `
+    )
+    .join("");
 };
